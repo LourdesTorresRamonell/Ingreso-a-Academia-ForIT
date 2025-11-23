516262e1db29a0a8dd08b068fac23ed0d789e10c
Lourdes Torres <luli2000cuenta2.0@gmail.com>
2025-11-23 20:55:49 -0300

    Agrego el codigo del server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 const express=require("express"); //importamos el express para poder utilizarlo
+const { controller }=require("./controllers/tareasController");
+const { router }=require("./routers");
+
 const app=express();
 const port=5000;
-app.get("/", (req, res)=>
-{
-    res.send("Hola");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.send("Bienvenido a la app");
 });
+
+app.use(`/api/task`, router);
 
 app.listen(port, ()=>{
     console.log(`Servidor escuchando al puerto ${port}`);
